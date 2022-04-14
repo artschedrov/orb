5257404333a418e413e6b929e2c464320bba2b2f
Art Schedrov <a.sch@tutanota.com>
2022-04-14 08:08:57 +0700

    add CRUD 2

--- server/src/main/java/com/example/server/ServerApplication.java
+++ server/src/main/java/com/example/server/ServerApplication.java
@@ -1,5 +1,9 @@
 package com.example.server;
 
+import com.example.server.enumeration.Status;
+import com.example.server.model.Server;
+import com.example.server.repo.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +12,20 @@ public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	CommandLineRunner run(ServerRepo serverRepo) {
+		return args -> {
+			serverRepo.save(new Server(
+					null,
+					"192.168.1.160",
+					"Fedora Server",
+					"16 GB",
+					"Virtual server",
+					"http://localhost:8080/server/image/server.png",
+					Status.SERVER_UP
+			));
+		};
 	}
 
 }
