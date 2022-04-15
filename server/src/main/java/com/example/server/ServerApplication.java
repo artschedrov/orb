a33c639495408736b05ab6079427b4dfc66d9751
Art Schedrov <a.sch@tutanota.com>
2022-04-16 04:14:12 +0700

    fast fix2

--- server/src/main/java/com/example/server/ServerApplication.java
+++ server/src/main/java/com/example/server/ServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -16,6 +17,7 @@ public class ServerApplication {
 
 	}
 
+	@Bean
 	CommandLineRunner run(ServerRepo serverRepo) {
 		return args -> {
 			serverRepo.save(new Server(
