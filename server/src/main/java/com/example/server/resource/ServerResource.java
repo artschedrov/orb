53d94dad0965b4318ee5cf5914bc5c9dc3660cc8
Art Schedrov <a.sch@tutanota.com>
2022-04-24 16:46:49 +0700

    mini fix

--- server/src/main/java/com/example/server/resource/ServerResource.java
+++ server/src/main/java/com/example/server/resource/ServerResource.java
@@ -15,6 +15,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static java.time.LocalDateTime.now;
 import static java.util.Map.of;
@@ -29,7 +30,8 @@ public class ServerResource {
     private final ServerImplementation serverService;
 
     @GetMapping("/list")
-    public ResponseEntity<Response> getServers() {
+    public ResponseEntity<Response> getServers() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(3);
         return ResponseEntity.ok(
                 Response.builder()
                     .timeStamp(now())
