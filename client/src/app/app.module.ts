e63c22e8ccc760c255b691c62ac58efe2919d9eb
Art Schedrov <a.sch@tutanota.com>
2022-04-26 00:34:25 +0700

    fix simple layout

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,8 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button'
-import { NgxLoadingButtonsModule } from 'ngx-loading-buttons';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 
 @NgModule({
   declarations: [
@@ -19,8 +18,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     HttpClientModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    NgxLoadingButtonsModule,
-    FontAwesomeModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
